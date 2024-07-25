9641943d6c7283de8d712d3b2e382e7dbdd5cdcd
xscho <lina.che.19@bk.ru>
2024-07-25 16:14:32 +0300

    Добавьте файлы проекта.

---
+++ InterfacePractices/Program.cs
@@ -0,0 +1,53 @@
+﻿// Определение интерфейса
+public interface IWrite
+{
+    void Write();
+}
+
+// Реализация интерфейса логгера
+public class ConsoleLogger : IWrite
+{
+    public void Write()
+    {
+        Console.WriteLine("Hello! I am Writer!");
+    }
+}
+
+// Мини-калькулятор
+public class MiniCalculator
+{
+    private readonly IWrite _logger;
+
+    public MiniCalculator(IWrite logger)
+    {
+        _logger = logger;
+    }
+
+    public int Add(int a, int b)
+    {
+        _logger.Write();
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        _logger.Write();
+        return a - b;
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IWrite logger = new ConsoleLogger();
+        MiniCalculator calculator = new MiniCalculator(logger);
+
+        int sum = calculator.Add(5, 3);
+        Console.WriteLine($"Sum: {sum}");
+
+        int difference = calculator.Subtract(5, 3);
+        Console.WriteLine($"Difference: {difference}");
+    }
+}
